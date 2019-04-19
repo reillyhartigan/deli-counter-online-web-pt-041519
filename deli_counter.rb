94b604711d0624aef49bde4cc966daa7cdcc6804
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-19 23:54:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,3 +7,9 @@ def line(array)
     puts "The line is currently"
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  position = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
