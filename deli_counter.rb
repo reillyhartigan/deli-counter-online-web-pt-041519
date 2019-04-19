db49f39ad548929b04c2ab8e296186899c50a2da
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-19 23:34:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+require 'pry'
+
+def line(array)
+  if array.length == "0"
+    puts "The line is currently empty"
+    binding.pry
+  else
+    puts "The line is currently"
+  end
+end
