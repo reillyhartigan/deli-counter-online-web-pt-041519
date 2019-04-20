9bfcf22e8504d4e11e146d8d4b316be8380c10be
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 00:23:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 
-def line(array)
-  if array.empty?
+def line(katz_deli)
+  if katz_deli.empty?
     puts "The line is currently empty"
   else
+    puts " "
   end
 end
 
@@ -13,6 +14,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{position} in line."
 end
 
-def now_serving(array)
+def now_serving(katz_deli)
 
 end
