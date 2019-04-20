37f2368378078f3a1ff907ea26c27afa6f0b3234
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 00:18:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == "0"
     puts "The line is currently empty"
   else
-    line = katz_deli.join(katz_deli.index)
+    line = katz_deli.join(katz_deli.index + ".")
     puts "The line is currently" + line
   end
 end
