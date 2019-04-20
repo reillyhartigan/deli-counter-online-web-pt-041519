1aa0f9aa0b90321a3f7d0428152ea53d1372feca
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 00:30:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,7 @@ def now_serving(katz_deli)
  if katz_deli.empty?
    puts "There is nobody waiting to be served!"
  else
+   serving = katz_deli.shift
+   puts "Currently serving #{serving}"
  end
 end
