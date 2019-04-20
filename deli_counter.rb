a605c81cfcf0fefa67b448fe8e8e7b27083453af
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 00:35:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,11 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    puts " "
+    line = "The line is currently:"
+    katz_deli.each.with_index(1) do |name, i|
+      line << " #{i},. #{person}"
   end
+  puts line
 end
 
 def take_a_number(katz_deli, name)
