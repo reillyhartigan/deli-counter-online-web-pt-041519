3e51cc7f63ca24e8d44d1322140174f2f237db3c
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 00:14:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
-def line(array)
-  if array.length == "0"
+def line(katz_deli)
+  if katz_deli.length == "0"
     puts "The line is currently empty"
   else
     puts "The line is currently"
