801243e01570e4956b061f68eb0ec0e5377d9e4e
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 00:39:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   else
     current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |name, i|
-      current_line << " #{i},. #{person}"
+      current_line << " #{i},. #{name}"
   end
   puts current_line
 end
